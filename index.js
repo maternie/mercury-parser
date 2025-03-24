aee44d124f93f4c3b4a0dc095e984b411f67992c
Meghan McCarthy <meghan@maternie.com>
2025-03-24 14:09:43 -0400

    Updated headers to bypass scraper blocks

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ app.get('/parser', async (req, res) => {
     if (!url) return res.status(400).json({ error: 'Missing URL' });
     const result = await Mercury.parse(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Referer': 'https://www.google.com'
       }
     });
     res.json(result);
